9e0e9deaa53b423112927d5e621492afa2d21197
Zagaceta123 <rz_sebas@hotmail.com>
2023-10-01 15:30:35 -0500

    Nuevo proyecto

---
+++ src/main/java/pe/edu/cibertec/appwebfragments/AppwebFragmentsApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebfragments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppwebFragmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppwebFragmentsApplication.class, args);
+	}
+
+}
